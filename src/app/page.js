9bf6e55f6665c608e9007b2c87ee49998815d669
spaceforworklinkers <anjulforwork@gmail.com>
2025-12-06 15:20:01 +0530

    first

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-4xl font-semibold m-10' >Welcome to the Home Page</h1>
+      <p>This is the main landing page of the application.</p>
+    </div>
+  )
+}
+
+export default page
